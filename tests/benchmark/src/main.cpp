d565823085b00b469832fa2df4fff28f12bb3930
jose <jabelcastellanosjoo@unm.edu>
2021-04-17 02:30:59 -0600

    Strength number of input handling and checking existance of file in benchmark script.

--- tests/benchmark/src/main.cpp
+++ tests/benchmark/src/main.cpp
@@ -1,9 +1,26 @@
 #include "UAutomizerFileReader.h"
+#include <sys/stat.h>
 
-int main(){
+inline bool exists_file (const std::string& name) {
+  struct stat buffer;   
+  return (stat (name.c_str(), &buffer) == 0); 
+}
+
+int main(int argc, char * argv[]){
+
+  if(argc != 2){
+    std::cerr << "Invalid number of inputs.\n";
+    return 1;
+  }
+
+  char const * file = argv[1];
+  if(!exists_file(file)){
+    std::cerr << "File doesnt exists.\n";
+    return 1;
+  }
 
   UAutomizerFileReader reader;
-  reader.process("/home/jose/relax-1.c_valid-memsafety.prp.smt2");
+  reader.process(file);
 
   return 0;
 }
