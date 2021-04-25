174b14c85276a762926358011782afbcaecca0a0
jose <jabelcastellanosjoo@unm.edu>
2021-04-24 19:41:42 -0600

    Add scripts for running the benchmark.

---
+++ tests/benchmark/scripts/execute-benchmark-reachsafety-mathsat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERIFICATION_FILES_DIR=/media/Documents/MaxDiff-Experiments/verification-files
+CURRENT_DIR=$(pwd)
+BENCHMARK_DIR=$(dirname $CURRENT_DIR)
+OUTPUT_FILE=$BENCHMARK_DIR/benchmark_reachsafety_results.txt
+
+[ ! -f $OUTPUT_FILE] && touch $OUTPUT_FILE
+make -j8 -C $BENCHMARK_DIR bin/benchmark
+
+for file in $VERIFICATION_FILES_DIR/ReachSafety-Arrays/*/*.smt2; do
+  pushd $BENCHMARK_DIR >> /dev/null
+  ./bin/benchmark $file 1 $OUTPUT_FILE
+  popd >> /dev/null
+done
