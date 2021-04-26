5732c8ba3c274dc80da1c3f43f84c1e45b4c1ed7
jose <jabelcastellanosjoo@unm.edu>
2021-04-25 23:54:54 -0600

    Keep working on processing results.

--- tests/benchmark/results/extract-results.py
+++ tests/benchmark/results/extract-results.py
@@ -5,28 +5,44 @@ import subprocess
 
 class ResultReader:
 
-    def __init__(self, file_name):
+    def add(self, file_name):
         self.file_name = file_name
-
-    def wat(self):
+        proc = subprocess.Popen([f"ls {file_name}"], stdout=subprocess.PIPE, shell=True)
+        directories = str(proc.communicate()[0])[2:-1]
+        for directory in directories.split("\\n"):
+            if(directory == ''):
+                continue
+            print(f"Directory: {directory}")
+            proc = subprocess.Popen([f"[ -d {directory} ]"], stdout=subprocess.PIPE, shell=True)
+            proc.communicate()
+            if(proc.returncode == 0):
+                proc = subprocess.Popen([f"ls {directory}"], stdout=subprocess.PIPE, shell=True)
+                files = str(proc.communicate()[0])[2:-1]
+                for _file in files.split("\\n"):
+                    if(_file == ''):
+                        continue
+                    print(f"file: {_file}")
         print(self.file_name)
 
 if __name__ == "__main__":
 
-    # r = ResultReader("haha")
-    # r.wat()
+    r = ResultReader()
+    r.add("/media/Documents/MaxDiff-Experiments/verification-files/MemSafety-Arrays")
 
-    # os.system("ls /media/Documents/MaxDiff-Experiments/verification-files")
-    proc = subprocess.Popen(["ls"], stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    hmm = str(out)[2:-1]
-    print(hmm)
-    print(f"{hmm}")
-    for x in hmm.split("\\n"):
-        proc = subprocess.Popen([f"[ -d {x} ]"], stdout=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
-        if(proc.returncode == 0):
-            proc = subprocess.Popen([f"ls {x}"], stdout=subprocess.PIPE, shell=True)
-            (out, err) = proc.communicate()
+    # proc = subprocess.Popen(["ls"], stdout=subprocess.PIPE, shell=True)
+    # directories = str(proc.communicate()[0])[2:-1]
+    # for directory in directories.split("\\n"):
+        # if(directory == ''):
+            # continue
+        # print(f"directory: {directory}")
+        # proc = subprocess.Popen([f"[ -d {directory} ]"], stdout=subprocess.PIPE, shell=True)
+        # proc.communicate()
+        # if(proc.returncode == 0):
+            # proc = subprocess.Popen([f"ls {directory}"], stdout=subprocess.PIPE, shell=True)
+            # files = str(proc.communicate()[0])[2:-1]
+            # for _file in files.split("\\n"):
+                # if(_file == ''):
+                    # continue
+                # print(f"file: {_file}")
 
 
