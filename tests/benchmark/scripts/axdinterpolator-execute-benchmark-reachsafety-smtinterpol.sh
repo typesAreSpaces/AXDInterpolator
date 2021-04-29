0d815ddf8ff1b832f2546d56eed5bce0b4e0c215
jose <jabelcastellanosjoo@unm.edu>
2021-04-29 13:38:40 -0600

    Fix scripts for the reachsafety track for axdinterpolator.

--- tests/benchmark/scripts/axdinterpolator-execute-benchmark-reachsafety-smtinterpol.sh
+++ tests/benchmark/scripts/axdinterpolator-execute-benchmark-reachsafety-smtinterpol.sh
@@ -3,7 +3,7 @@
 VERIFICATION_FILES_DIR=/media/Documents/MaxDiff-Experiments/verification-files
 CURRENT_DIR=$(pwd)
 BENCHMARK_DIR=$(dirname $CURRENT_DIR)
-OUTPUT_FILE=$BENCHMARK_DIR/benchmark_memsafety_results-axdinterpolator.txt
+OUTPUT_FILE=$BENCHMARK_DIR/benchmark_reachsafety_results-axdinterpolator.txt
 
 [ ! -f $OUTPUT_FILE ] && touch $OUTPUT_FILE
 make -j8 -C $BENCHMARK_DIR bin/benchmark
