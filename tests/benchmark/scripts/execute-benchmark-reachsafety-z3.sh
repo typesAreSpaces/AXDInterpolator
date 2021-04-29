b3e28c8b2175e62665953f15c6cb438be1b97b44
jose <jabelcastellanosjoo@unm.edu>
2021-04-28 23:00:15 -0600

    Fix scripts for benchmarking.

--- tests/benchmark/scripts/execute-benchmark-reachsafety-z3.sh
+++ tests/benchmark/scripts/execute-benchmark-reachsafety-z3.sh
@@ -5,7 +5,7 @@ CURRENT_DIR=$(pwd)
 BENCHMARK_DIR=$(dirname $CURRENT_DIR)
 OUTPUT_FILE=$BENCHMARK_DIR/benchmark_reachsafety_results-z3.txt
 
-[ ! -f $OUTPUT_FILE] && touch $OUTPUT_FILE
+[ ! -f $OUTPUT_FILE ] && touch $OUTPUT_FILE
 make -j8 -C $BENCHMARK_DIR bin/benchmark
 
 for file in $VERIFICATION_FILES_DIR/ReachSafety-Arrays/*/*.smt2; do
