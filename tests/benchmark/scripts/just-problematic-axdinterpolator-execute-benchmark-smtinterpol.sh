28cfc5b7bacee2d02851fc590137923c9ccb8b6e
jose <jabelcastellanosjoo@unm.edu>
2021-04-30 04:16:37 -0600

    Add just problematic files script.

---
+++ tests/benchmark/scripts/just-problematic-axdinterpolator-execute-benchmark-smtinterpol.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERIFICATION_FILES_DIR=/media/Documents/MaxDiff-Experiments/verification-files
+CURRENT_DIR=$(pwd)
+BENCHMARK_DIR=$(dirname $CURRENT_DIR)
+OUTPUT_FILE=$BENCHMARK_DIR/benchmark_memsafety_results-axdinterpolator.txt
+
+[ ! -f $OUTPUT_FILE ] && touch $OUTPUT_FILE
+make -j8 -C $BENCHMARK_DIR bin/benchmark
+
+for file in $VERIFICATION_FILES_DIR/*.smt2; do
+  pushd $BENCHMARK_DIR >> /dev/null
+  ./bin/benchmark $file 2 $OUTPUT_FILE 0
+  popd >> /dev/null
+done
