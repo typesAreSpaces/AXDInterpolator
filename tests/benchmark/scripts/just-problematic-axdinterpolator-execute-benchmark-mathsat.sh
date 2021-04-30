ec0f07fbcffec2a88806c01fd9fbf8a527c31ac6
jose <jabelcastellanosjoo@unm.edu>
2021-04-30 04:34:35 -0600

    Fix problematic files script.

--- tests/benchmark/scripts/just-problematic-axdinterpolator-execute-benchmark-mathsat.sh
+++ tests/benchmark/scripts/just-problematic-axdinterpolator-execute-benchmark-mathsat.sh
@@ -3,12 +3,12 @@
 VERIFICATION_FILES_DIR=/home/jose/Documents/GithubProjects/AXDInterpolator/tests/problematic_files
 CURRENT_DIR=$(pwd)
 BENCHMARK_DIR=$(dirname $CURRENT_DIR)
-OUTPUT_FILE=$BENCHMARK_DIR/benchmark_memsafety_results-axdinterpolator.txt
+OUTPUT_FILE=$BENCHMARK_DIR/benchmark_problematic_results-axdinterpolator.txt
 
 [ ! -f $OUTPUT_FILE ] && touch $OUTPUT_FILE
 make -j8 -C $BENCHMARK_DIR bin/benchmark
 
-for file in $VERIFICATION_FILES_DIR/*.smt2; do
+for file in $VERIFICATION_FILES_DIR/.*smt2*; do
   pushd $BENCHMARK_DIR >> /dev/null
   ./bin/benchmark $file 1 $OUTPUT_FILE 0
   popd >> /dev/null
