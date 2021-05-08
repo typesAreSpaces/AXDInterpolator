6143efacf55dd9fb5f67e0265a7e26bc41359459
jose <jabelcastellanosjoo@unm.edu>
2021-05-08 03:06:06 -0600

    Update scripts to benchmark project.

--- tests/benchmark/scripts/axdinterpolator-execute-benchmark-reachsafety-smtinterpol.sh
+++ tests/benchmark/scripts/axdinterpolator-execute-benchmark-reachsafety-smtinterpol.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VERIFICATION_FILES_DIR=/media/Documents/MaxDiff-Experiments/verification-files
 CURRENT_DIR=$(pwd)
 BENCHMARK_DIR=$(dirname $CURRENT_DIR)
 OUTPUT_FILE=$BENCHMARK_DIR/benchmark_reachsafety_results-axdinterpolator.txt
+VERIFICATION_FILES_DIR=$(dirname $BENCHMARK_DIR)/verification-files/files
 
 [ ! -f $OUTPUT_FILE ] && touch $OUTPUT_FILE
 make -j8 -C $BENCHMARK_DIR bin/benchmark
