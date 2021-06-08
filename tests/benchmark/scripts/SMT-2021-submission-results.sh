ee6bfc9f2066b7911467198299f7230ff21fe98e
jose <jabelcastellanosjoo@unm.edu>
2021-06-07 18:54:27 -0600

    Minor changes.

--- tests/benchmark/scripts/SMT-2021-submission-results.sh
+++ tests/benchmark/scripts/SMT-2021-submission-results.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-cd ../../../ \
-  && [ -z "$(ls -A ./dependencies/z3-interp-plus)" ] \
-  && git submodule update --init --remote dependencies/z3-interp-plus
-make -C ../ -j8 bin/benchmark
-unzip ../../verification-files/files.zip
-mv files ../../verification-files/
+cd ../../../
+[ -z "$(ls -A ./dependencies/z3-interp-plus)" ] && git submodule update --init --remote dependencies/z3-interp-plus
+cd ./tests/verification-files
+[ ! -d files ] && unzip files.zip
+cd ../benchmark
+make -j8 bin/benchmark
+cd ./scripts
 
 CURRENT_DIR=$(pwd)
 BENCHMARK_DIR=$(dirname $CURRENT_DIR)
