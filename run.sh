7d0689821af1894cebcee173f557fc72906180b3
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2023-02-21 17:14:18 -0700

    Use /usr/bin/env bash in header of run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-#! /usr/bin/bash
+#!/usr/bin/env bash
 
 CURRENT_DIR=$(pwd)
+LDIR=$CURRENT_DIR/lib
+DYLD_LIBRARY_PATH=$LDIR
+export DYLD_LIBRARY_PATH
 
 $CURRENT_DIR/bin/axd_interpolator \
   QF_LIA \
