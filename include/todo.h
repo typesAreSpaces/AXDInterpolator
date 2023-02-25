0624bdcc57f46e6aad9e6000eabcd0a598f62cbb
abbypribis <abbypribis@gmail.com>
2023-02-24 22:27:22 -0700

    added function definitions

--- include/todo.h
+++ include/todo.h
@@ -5,8 +5,16 @@
 
 void traverse(z3::expr);
 
-void test();
+void M_O_instantiation(z3::expr, const z3::expr_vector &,
+                       const z3::expr_vector &);
 
-void M_O_instantiation(z3::expr, const z3::expr_vector &, const z3::expr_vector &);
+bool compare_expr(z3::expr, z3::expr);
+
+void flattening_helper(z3::expr, std::vector<z3::expr> &,
+                       std::vector<z3::expr> &);
+
+z3::expr get_new_variable(z3::expr, z3::context &, int &);
+
+std::vector<z3::expr> flattening(z3::expr, z3::context &);
 
 #endif /* TODO_H */
