28bc1ee6d09409b8a98b40e1cd36230dfd6adbdb
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2023-04-04 02:19:54 -0600

    Minor changes.

--- run.sh
+++ run.sh
@@ -6,10 +6,15 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     export DYLD_LIBRARY_PATH
 fi
 
+iZ3=0
+MathSAT=1
+SMTInterpolator=2
+INTERPOLATION_ENGINE=$iZ3
+
 THEORY=QF_LIA
-TEST_DIR=tests/smt2-files/caxd_examples/
-CURR_TEST=${TEST_DIR}/6_2.smt2
-INTERPOLATION_ENGINE=0
+
+TEST_DIR=tests/smt2-files/caxd_examples
+CURR_TEST=$TEST_DIR/6_2.smt2
 
 ./bin/axd_interpolator \
     $THEORY $CURR_TEST \
