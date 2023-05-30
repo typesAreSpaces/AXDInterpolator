9ddcb3ce50a1de1dfec7ff7f734579a7725faf64
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2023-05-30 12:11:21 -0600

    Review benchmarking scripts.

--- tests/benchmark/scripts/SMT-2021-submission-results.sh
+++ tests/benchmark/scripts/SMT-2021-submission-results.sh
@@ -5,23 +5,37 @@ BENCHMARK_DIR=$(dirname $CURRENT_DIR)
 TESTS_DIR=$(dirname $BENCHMARK_DIR)
 VERIFICATION_FILES_DIR=$TESTS_DIR/verification-files/files
 
+# Unzip 'raw' verification files
+# in ../../verification-files/files
+# if such directory is not present
 [ ! -d $VERIFICATION_FILES_DIR ] \
   && unzip $VERIFICATION_FILES_DIR.zip \
   && mv files $VERIFICATION_FILES_DIR
 
 ulimit -St 360 -Sv 4500000
 
+# Move to root of project
 cd ../../../
+
+# Abort if mathsat bin is not present
 [ ! -f ./bin/mathsat ] && echo "Mathsat not in local bin folder" && cd ./tests/benchmark/scripts && exit
 
 echo "How many cores can be used? 1, 3, or 6"
 read num_of_cores_allowed
 
+# Download z3-interp-plus if not present
 [ -z "$(ls -A ./dependencies/z3-interp-plus)" ] && git submodule update --init --remote dependencies/z3-interp-plus
+
 cd ./tests/verification-files
 [ ! -d files ] && unzip files.zip
+
+# Return to original directory
 cd ../benchmark
+
+# Execute main benchmark script
 make -j8 bin/benchmark
+
+# Move to 'scripts' directory to post-process the results
 cd ./scripts
 
 if [ "$num_of_cores_allowed" == "1" ]; then
@@ -107,19 +121,32 @@ if [ "$num_of_cores_allowed" == "3" ]; then
 fi
 
 if [ "$num_of_cores_allowed" == "6" ]; then
+  # Execute scripts to test axdinterpolant using z3
+  # for the memsafety track
   ./axdinterpolator-execute-benchmark-memsafety-z3.sh &
   pid_memsafety_z3=$!
+  # Execute scripts to test axdinterpolant using mathsat
+  # for the memsafety track
   ./axdinterpolator-execute-benchmark-memsafety-mathsat.sh &
   pid_memsafety_mathsat=$!
+  # Execute scripts to test axdinterpolant using smtinterpol
+  # for the memsafety track
   ./axdinterpolator-execute-benchmark-memsafety-smtinterpol.sh &
   pid_memsafety_smtinterpol=$!
+  # Execute scripts to test axdinterpolant using z3
+  # for the reachsafety track
   ./axdinterpolator-execute-benchmark-reachsafety-z3.sh &
   pid_reachsafety_z3=$!
+  # Execute scripts to test axdinterpolant using mathsat
+  # for the reachsafety track
   ./axdinterpolator-execute-benchmark-reachsafety-mathsat.sh &
   pid_reachsafety_mathsat=$!
+  # Execute scripts to test axdinterpolant using smtinterpol
+  # for the reachsafety track
   ./axdinterpolator-execute-benchmark-reachsafety-smtinterpol.sh &
   pid_reachsafety_smtinterpol=$!
 
+  # Join for the async executions to finish
   wait $pid_memsafety_z3
   wait $pid_memsafety_mathsat
   wait $pid_memsafety_smtinterpol
@@ -127,24 +154,38 @@ if [ "$num_of_cores_allowed" == "6" ]; then
   wait $pid_reachsafety_mathsat
   wait $pid_reachsafety_smtinterpol
 
+  # Rename stuff (?)
   cat $BENCHMARK_DIR/benchmark_memsafety_results-* > $BENCHMARK_DIR/benchmark_memsafety_results_axdinterpolator.txt
   cat $BENCHMARK_DIR/benchmark_reachsafety_results-* > $BENCHMARK_DIR/benchmark_reachsafety_results_axdinterpolator.txt
   rm -rf $BENCHMARK_DIR/benchmark_memsafety_results-*
   rm -rf $BENCHMARK_DIR/benchmark_reachsafety_results-*
 
+  # Execute scripts to test z3
+  # for the memsafety track
   ./execute-benchmark-memsafety-z3.sh &
   pid_memsafety_z3=$!
+  # Execute scripts to test mathsat
+  # for the memsafety track
   ./execute-benchmark-memsafety-mathsat.sh &
   pid_memsafety_mathsat=$!
+  # Execute scripts to test smtinterpol
+  # for the memsafety track
   ./execute-benchmark-memsafety-smtinterpol.sh &
   pid_memsafety_smtinterpol=$!
+  # Execute scripts to test z3
+  # for the reachsafety track
   ./execute-benchmark-reachsafety-z3.sh &
   pid_reachsafety_z3=$!
+  # Execute scripts to test mathsat
+  # for the reachsafety track
   ./execute-benchmark-reachsafety-mathsat.sh &
   pid_reachsafety_mathsat=$!
+  # Execute scripts to test smtinterpol
+  # for the reachsafety track
   ./execute-benchmark-reachsafety-smtinterpol.sh &
   pid_reachsafety_smtinterpol=$!
 
+  # Join for the async executions to finish
   wait $pid_memsafety_z3
   wait $pid_memsafety_mathsat
   wait $pid_memsafety_smtinterpol
@@ -152,18 +193,21 @@ if [ "$num_of_cores_allowed" == "6" ]; then
   wait $pid_reachsafety_mathsat
   wait $pid_reachsafety_smtinterpol
 
+  # Rename stuff (?)
   cat $BENCHMARK_DIR/benchmark_memsafety_results-* > $BENCHMARK_DIR/benchmark_memsafety_results_other-solvers.txt
   cat $BENCHMARK_DIR/benchmark_reachsafety_results-* > $BENCHMARK_DIR/benchmark_reachsafety_results_other-solvers.txt
   rm -rf $BENCHMARK_DIR/benchmark_memsafety_results-*
   rm -rf $BENCHMARK_DIR/benchmark_reachsafety_results-*
 fi
 
+# Rename stuff (?)
 cat $BENCHMARK_DIR/benchmark_memsafety_results_* > $BENCHMARK_DIR/benchmark_memsafety_results.txt
 cat $BENCHMARK_DIR/benchmark_reachsafety_results_* > $BENCHMARK_DIR/benchmark_reachsafety_results.txt
 
 rm $BENCHMARK_DIR/benchmark_memsafety_results_*
 rm $BENCHMARK_DIR/benchmark_reachsafety_results_*
 
+# Post-processing script
 ./process-results/SMT-2021-submission-results.py $TESTS_DIR/verification-files/files $BENCHMARK_DIR
 
 rm $BENCHMARK_DIR/benchmark_memsafety_results.txt
