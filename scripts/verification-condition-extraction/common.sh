f291adfccbeae3af527ffefa26d179a23f02baa1
jose <jabelcastellanosjoo@unm.edu>
2023-06-10 17:33:19 -0600

    Finish working on verification condition extraction scripts.

--- scripts/verification-condition-extraction/common.sh
+++ scripts/verification-condition-extraction/common.sh
@@ -3,4 +3,4 @@
 SV_COMP_DIR=$PWD/../sv-benchmarks/c
 ULTIMATE_DIR=$HOME/Documents/GithubProjects/ultimate/releaseScripts/default/UAutomizer-linux
 # TODO: Increase the timeout duration
-TIMEOUT_CMD='timeout 1'
+TIMEOUT_CMD='timeout 900'
