2b5018e5809ba845ee883c1e18cf7a643b2c226e
jose <jabelcastellanosjoo@unm.edu>
2023-06-13 14:14:25 -0600

    Add debug.sh script in tests/benchmark ...
    ... to identify a file which triggers the
    bug in caxdinterpolator.

---
+++ tests/benchmark/debug.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./bin/benchmark /home/jose/Documents/GithubProjects/AXDInterpolator/extension/tests/smt2-files/benchmark/files/ReachSafety-Arrays/array-fpi/brs2f.c_unreach-call.prp.smt2 0 here.txt 0
