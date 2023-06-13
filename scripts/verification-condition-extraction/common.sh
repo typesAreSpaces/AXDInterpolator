7c8140075b8a240450c121275b42d1279f02861b
jose <jabelcastellanosjoo@unm.edu>
2023-06-13 12:47:40 -0600

    Add scripts/verification-condition-extraction/sv-benchmarks

--- scripts/verification-condition-extraction/common.sh
+++ scripts/verification-condition-extraction/common.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-SV_COMP_DIR=$PWD/../sv-benchmarks/c
+SV_COMP_DIR=$PWD/sv-benchmarks/c
 ULTIMATE_DIR=$HOME/Documents/GithubProjects/ultimate/releaseScripts/default/UAutomizer-linux
-# TODO: Increase the timeout duration
 TIMEOUT_CMD='timeout 900'
