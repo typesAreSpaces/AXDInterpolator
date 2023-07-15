a8c778e9fffea7252c3ed5007167994f6701a6de
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2023-07-14 22:34:08 -0600

    Organize files.

--- debug_caxdinterpolator_smtinterpol.sh
+++ debug_caxdinterpolator_smtinterpol.sh
@@ -2,7 +2,8 @@
 
 ulimit -St 360 -Sv 4500000
 
-for FILE in ./tests/smt2-files/problematic_files_caxdinterpolator_smtinterpol/*.smt2; do
+#for FILE in ./tests/smt2-files/problematic_files_caxdinterpolator_smtinterpol/*.smt2; do
+for FILE in ./tests/smt2-files/problematic_files_caxdinterpolator_smtinterpol/check/*.smt2; do
     echo "Processing $FILE"
     time ./bin/caxd_interpolator QF_LIA $FILE 2
 done
--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ fi
 iZ3=0
 MathSAT=1
 SMTInterpolator=2
-INTERPOLATION_ENGINE=$iZ3
+INTERPOLATION_ENGINE=$SMTInterpolator
 
 THEORY=QF_LIA
 
@@ -25,8 +25,11 @@ THEORY=QF_LIA
 #CURR_TEST=axdinterpolator_Z3_buggy2-simplified-2.smt2
 #CURR_TEST=axdinterpolator_Z3_brs4f.c_unreach-call.prp.smt2 
 
-TEST_DIR=tests/smt2-files/buggy-benchmark2
-CURR_TEST=buggy-post-benchmark.smt2
+#TEST_DIR=tests/smt2-files/buggy-benchmark2
+#CURR_TEST=buggy-post-benchmark.smt2
+
+TEST_DIR=tests/smt2-files/problematic_files_caxdinterpolator_smtinterpol/check
+CURR_TEST=debug_this_156.smt2
 
 echo ">> Checking file" "$TEST_DIR/$CURR_TEST"
 
