19bf381e9d491b5029a80751ea03dca68f2b0f4c
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2023-07-16 23:42:59 -0600

    Organize scripts.

--- tests/benchmark/scripts/pcomf_debug.sh
+++ tests/benchmark/scripts/pcomf_debug.sh
@@ -5,6 +5,6 @@ OUTPUT_FILE=debugging_pcompf.c_unreach-call.prp.txt
 ulimit -St 360 -Sv 4500000
 
 [ ! -f $OUTPUT_FILE ] && touch $OUTPUT_FILE
-./bin/benchmark ../../tests/smt2-files/benchmark/files/ReachSafety-Arrays/array-fpi/pcompf.c_unreach-call.prp.smt2 2 here.txt 0  
+./../bin/benchmark ../../../tests/smt2-files/benchmark/files/ReachSafety-Arrays/array-fpi/pcompf.c_unreach-call.prp.smt2 2 $OUTPUT_FILE 0  
 
 ulimit -St unlimited -Sv unlimited
