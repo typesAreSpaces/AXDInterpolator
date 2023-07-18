b5516a2c1bf21fb41046584a929690f87244ff8b
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2023-07-18 12:53:06 -0600

    Update debug and run script.

--- debug.sh
+++ debug.sh
@@ -13,9 +13,9 @@ INTERPOLATION_ENGINE=0
 
 THEORY=QF_LIA
 
-TEST_DIR=tests/smt2-files/caxd_examples
+#TEST_DIR=tests/smt2-files/caxd_examples
 #CURR_TEST=7_2_2.smt2
-CURR_TEST=7_2.smt2
+#CURR_TEST=7_2.smt2
 
 #TEST_DIR=tests/smt2-files/buggy-benchmark
 #CURR_TEST=axdinterpolator_Z3_buggy2-simplified-2.smt2
@@ -27,9 +27,11 @@ CURR_TEST=7_2.smt2
 #TEST_DIR=tests/smt2-files/problematic_files_caxdinterpolator_smtinterpol/check
 #CURR_TEST=debug_this_156.smt2
 
+TEST_DIR=/home/jose
+CURR_TEST=debug_this_241.smt2
+
 echo ">> Debugging file" "$TEST_DIR/$CURR_TEST"
 
 gdb --args ./bin/caxd_interpolator \
     $THEORY $TEST_DIR/$CURR_TEST \
     $INTERPOLATION_ENGINE
-
--- run.sh
+++ run.sh
@@ -9,12 +9,12 @@ fi
 iZ3=0
 MathSAT=1
 SMTInterpolator=2
-INTERPOLATION_ENGINE=1
+INTERPOLATION_ENGINE=2
 
 THEORY=QF_LIA
 
-TEST_DIR=tests/smt2-files/caxd_examples
-CURR_TEST=6_8.smt2
+#TEST_DIR=tests/smt2-files/caxd_examples
+#CURR_TEST=6_8.smt2
 #CURR_TEST=7_1.smt2
 #CURR_TEST=7_2.smt2
 #CURR_TEST=not_7_2.smt2
@@ -31,8 +31,17 @@ CURR_TEST=6_8.smt2
 #TEST_DIR=tests/smt2-files/problematic_files_caxdinterpolator_smtinterpol/check
 #CURR_TEST=debug_this_156.smt2
 
+TEST_DIR=/home/jose
+CURR_TEST=debug_this_241.smt2
+
 echo ">> Checking file" "$TEST_DIR/$CURR_TEST"
 
-./bin/caxd_interpolator \
-    $THEORY $TEST_DIR/$CURR_TEST \
+# ./bin/caxd_interpolator \
+#     $THEORY $TEST_DIR/$CURR_TEST \
+#     $INTERPOLATION_ENGINE
+
+for x in $TEST_DIR/*.smt2; do
+    ./bin/caxd_interpolator \
+    $THEORY $x \
     $INTERPOLATION_ENGINE
+done;
